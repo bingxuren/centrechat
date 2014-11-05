4beb69ea1c39eaaee72963ec92f5be930ea299a7
ye.sheng <ye.sheng@centre.edu>
2014-11-05 12:03:53 -0500

    changed massive test

--- massiveChat.py
+++ massiveChat.py
@@ -5,17 +5,21 @@ import thread
 
 cList = []
 
-for i in range(3):
+for i in range(8):
 	cList.append(client("testClient" + str(i), "10.45.1.37"))
 	thread.start_new_thread(cList[i].handleMessages, ())
 time.sleep(1)
 
 for i in range(20):
 	for c in cList:
-		c.sendMessage("I am the best.")
-	time.sleep(0.3)
+		c.sendMessage(str(i))
+	time.sleep(0.6)
 
 for c in cList:
 	c.disconnectServer()
 time.sleep(2)
 
+for c in cList:
+	print "-------------"
+	for l in c.rcvBuffer:
+		print str(l[0]) + ":" + str(l[1])
